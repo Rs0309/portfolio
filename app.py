d248a7c70e3ef3d7ef927fbbc962de1561ee4c03
Rs0309 <ritviksingh003@gmail.com>
2025-04-11 19:28:51 +0530

    Update Flask config and add more workflow debugging

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='static')
 
 @app.route('/')
 def index():
